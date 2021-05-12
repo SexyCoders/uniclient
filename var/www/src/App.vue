3a80e5f46ed89e9b5a3f3497463c0d3668665dcf
Shane <sotiris4321@gmail.com>
2021-05-12 16:14:45 +0300

    added vue and started working on page - some errors with my custom scripts

---
+++ var/www/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <Menu />
+  <CustomerTable />
+  </div>
+</template>
+
+<script>
+import CustomerTable from './components/CustomerTable.vue'
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CustomerTable,
+    Menu
+  }
+}
+</script>
+
+<style scoped>
+  @import "../../../lib/css/sb-admin-2.min.css";
+</style>
---
+++ var/www/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import $ from "jquery"
+import './index.css'
+
+require("../../lib/js/sb-admin-2.min.js");
+
+createApp(App).mount('#app')
