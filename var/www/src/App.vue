d6347471598befd4303cc120c9daf255983286be
Shane <sotiris4321@gmail.com>
2021-05-18 11:17:25 +0300

    clean commit

---
+++ var/www/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+      <Head/>
+</template>
+
+<script>
+import Head from "../src/Head.vue";
+//import init from "../../../local/lib/js/libauth-runtime-modules.min.js";
+
+export default {
+  name: 'App',
+  components: {
+    Head
+  },
+  data() {
+  return{
+    kalhmera : [],
+    kalhspera : {}
+  }
+  },
+  methods : {
+  //egoeimaimalakas()
+      //{
+        //console.log("Eimai malakas eipame!");
+        //this.kalhmera.push("geia!");
+        //console.log(JSON.stringify(this.kalhmera));
+        //init();
+      //}
+  },
+  mounted() {
+    //this.$root.$on("HelloWorld",()=>{
+      // console.log("eimai malakas"); 
+    //});
+  },
+  beforeUnmount() {
+  },
+  computed : {
+  },
+
+
+  created()  {
+    //init();
+  }
+}
+
+
+
+</script>
+
+<style>
+
+  @import "../../../lib/css/sb-admin-2.min.css";
+</style>
