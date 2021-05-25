51db467f0ad8d4ccb346bcaeda771caaa98c39d0
Shane <sotiris4321@gmail.com>
2021-05-25 14:40:38 +0300

    working on final dual solution for handling MOVE and cors with max safety

---
+++ var/www/src/components/Lock.vue
@@ -0,0 +1,36 @@
+<template>
+	<overlay>
+        <span>My popup</span>
+    </overlay>
+</template>
+
+<script>
+import Overlay from "vuejs-overlay";
+
+export default {
+  name: 'Lock',
+  components: {
+	Overlay
+  },
+  data() {
+  return{
+  }
+  },
+  computed : {
+  },
+  mounted() {
+  },
+  beforeUnmount() {
+  },
+  created()  {
+  },
+  methods : {
+  }
+}
+
+
+
+</script>
+
+<style>
+</style>
