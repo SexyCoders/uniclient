d03e069653d53d6cb6dc74da9d084383443d6900
Shane <sotiris4321@gmail.com>
2021-05-26 15:46:18 +0300

    working on auth functions to work with the new logic

--- var/www/src/App.vue
+++ var/www/src/App.vue
@@ -1,42 +1,30 @@
 <template>
-  <!--<div v-if="AUTH">-->
-    <!--<Head />-->
-  <!--</div>-->
   <Lock />
 </template>
 
 <script>
-//import Head from "../src/Head.vue";
 import Lock from "../src/components/Lock.vue";
-import "../../../local/lib/js/libauth-runtime-modules.js";
+import { handshake } from "../../../local/lib/js/libauth-runtime-modules.js";
 
 export default {
   name: 'App',
   components: {
-    //Head,
     Lock
   },
   data() {
   return{
-    AUTH:0
   }
   },
   computed : {
   },
   mounted() {
-    this.test();
   },
   beforeUnmount() {
   },
   created()  {
+    //handshake(window.__auth_system);
   },
   methods : {
-    test()
-      {
-        console.log(this.AUTH);
-        this.AUTH=1;
-        console.log(this.AUTH);
-      }
   }
 }
 
@@ -44,6 +32,6 @@ export default {
 
 </script>
 
-<style>
-  @import "../../../lib/css/sb-admin-2.min.css";
-</style>
+<!--<style>-->
+  <!--@import "../../../lib/css/sb-admin-2.min.css";-->
+<!--</style>-->
--- var/www/src/main.js
+++ var/www/src/main.js
@@ -1,6 +1,5 @@
 import App from './App.vue';
 import { createApp } from 'vue';
-import './index.css';
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
