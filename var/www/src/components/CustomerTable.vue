0e1a973ab5017c7b597e69a17054838d2b80a409
Shane <sotiris4321@gmail.com>
2021-06-10 14:51:25 +0300

    added table now configuring

---
+++ var/www/src/components/CustomerTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="CustomerTable">
+    <v-grid
+      theme="compact"
+      :source="rows"
+      :columns="columns"
+    ></v-grid>
+  </div>
+</template>
+ 
+<script>
+import VGrid from "@revolist/vue3-datagrid";
+export default {
+  name: "CustomerTable",
+  data() {
+    return {
+      columns: [{ prop: "name",name:"malakies" }, { prop: "details" }],
+      rows: [{
+        name: "1",
+        details: "Item 1",
+      }]
+    };
+  },
+  components: {
+    VGrid
+  },
+};
+</script>
+
