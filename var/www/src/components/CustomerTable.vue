b249e1d083311f4be30b1fe20e14c0e978ebb670
Shane <sotiris4321@gmail.com>
2021-07-08 19:00:44 +0300

    created customer profile page and cleaned up old junk - will now implement errors fresh from scrap

--- var/www/src/components/CustomerTable.vue
+++ var/www/src/components/CustomerTable.vue
@@ -3,9 +3,13 @@
   <ag-grid-vue
     style="height: 500px"
     class="ag-theme-alpine"
+    id="myGrid"
     @grid-ready="onGridReady"
+    :gridOptions="gridOptions"
     :columnDefs="columnDefs"
     :modules="modules"
+    :rowSelection="rowSelection"
+    @selection-changed="onSelectionChanged"
   >
   </ag-grid-vue>
 </template>
@@ -33,13 +37,38 @@ export default {
   },
   data() {
     return {
-      columnDefs: cols,
+      gridOptions: null,
+      gridApi: null,
+      columnDefs: null,
       modules: [ClientSideRowModelModule],
+      rowSelection:null,
     };
   },
+  beforeMount(){
+    this.gridOptions={};
+    this.columnDefs=cols;
+    this.rowSelection = 'single';
+  },
   mounted() {
+    this.gridApi = this.gridOptions.api;
+    this.gridColumnApi = this.gridOptions.columnApi;
+    //this.check();
   },
   methods: {
+    check()
+      {
+      $.ajax({
+          type: 'POST',
+          url: window.__SCD.datacenter+"/get_pending_errors_all",
+          data: "",
+          success:
+          (response) =>
+              {
+                console.log(response);
+              },
+            async:false
+            });
+      },
     setTitle(title)
       {
         this.$store.page_title=title;
@@ -66,7 +95,16 @@ export default {
             async:false
             });
 
-      }
+      },
+    onSelectionChanged() {
+      var selectedRows = this.gridApi.getSelectedRows();
+      //this.$store.selection=selectedRows[0].ID;
+      //console.log(selectedRows);
+      var customer=Object.values(this.$store.customers).filter((customer)=>customer.ID==selectedRows[0].ID);
+      this.$store.selection=customer[0];
+      console.log(JSON.stringify(this.$store.selection));
+      this.$router.push('customerprofile');
+    },
     },
   created()
     {
