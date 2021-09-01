aed5f50737fb55ee7b857e3a645e4eba61ae7aac
Shane <sotiris4321@gmail.com>
2021-09-01 08:17:47 +0300

    cleaned config - added datacenter apache file - working on resuming development

--- testing.cc
+++
@@ -1,9 +0,0 @@
-#include <uniclient/microsun/m_database.h>
-#include <iostream>
-
-int main()
-	{
-		Microsun::Database test;
-		test.get("customer_all");
-		std::cout<<test.stored_customers[0].getCompany()<<"\n";
-	}
--- testing.php
+++
@@ -1,6 +0,0 @@
-<?php
-        $database=new MicrosunDatabase;
-        $to_return=new stdClass;
-        $to_return->count=$database->getPendingCount();
-        echo json_encode($to_return);
-?>
