db35b79af72ac4e8c78340742cdd3b400ecdccca
Shane <sotiris4321@gmail.com>
2021-09-07 18:26:35 +0300

    error manager and logger working perfectly - todo list on paper!

--- local/src/c++/microsun/plant_manager.cc
+++ local/src/c++/microsun/plant_manager.cc
@@ -2,25 +2,42 @@
 #include <m_database.h>
 #include <sqlite3.h>
 
-Php::Value Microsun::Database::storePlant(Php::Parameters &arg)
+static int callback_log(void* passthrough,int argc,char** argv,char** col)
     {
-        Microsun::Plant* plant= new Microsun::Plant();
-        plant->ID=arg[0].stringValue();
-        plant->Power=atof(arg[1]);
-        //plant->Owner=this->getCustomerByCompanyName(arg[2]);
-        //plant->county=this->getCountyByName(arg[3].stringValue());
-        plant->Borough=arg[4].stringValue();
-        plant->Location=arg[5].stringValue();
-        plant->Area=arg[6];
-        plant->NPanels=arg[7];
-        //plant->panel=this->getPanelByName(arg[8]);
-        plant->Strings=arg[9].stringValue();
-        //plant->Mounting=this->getMounterByName(arg[10]);
-        //plant->inverter=this->getInverterByModel(arg[11]);
-        //plant->constructor=this->getConstructorByCompany(arg[12]);
-        plant->ConnectionNumber=atol(arg[13]);
-        plant->ConnectionDate->fromString(arg[14].stringValue());
-        plant->TrackerBegin->fromString(arg[15].stringValue());
-        plant->SellPrice=atof(arg[16]);
-    return 123;
+        std::vector<Microsun::Problem*>* tmp=(std::vector<Microsun::Problem*>*) passthrough;
+        Microsun::Problem* T=new Microsun::Problem();
+        T->reg_id=atoi(*argv);
+        T->plant=(*tmp->begin())->database->getPlant(argv[1]);
+        T->Pos=atoi(argv[2]);
+        T->Type=argv[3];
+        T->ErrorCode=argv[4];
+        T->ReportedDate->fromString(argv[5]);
+        (*tmp->begin())->database->get_user(argv[6],"all",T->ReportedUser);
+        T->ErrorNotes=argv[7];
+        (*tmp)[0]->database->get_user(argv[8],"all",T->AssignedMech);
+        T->ResolvedDate->fromString(argv[9]);
+        T->MechNotes=argv[10];
+        tmp->push_back(T);
+    return 0;
     }
+
+
+Php::Value Microsun::Database::getPlantLog(Php::Parameters &arg)
+    {
+        char* errmsg;
+        std::string sql;
+        sqlite3* db;
+        std::vector<Microsun::Problem*> tmp;
+        Microsun::Problem* dummy=new Microsun::Problem(this);
+        tmp.push_back(dummy);
+        sql="SELECT * FROM "+arg[0].stringValue()+" ORDER BY ID DESC LIMIT 30;";
+        sqlite3_open(this->ErrorPath.c_str(),&db);
+        int check=sqlite3_exec(db, sql.c_str(),callback_log,(void*)&tmp,&errmsg);
+        sqlite3_close(db);
+        tmp.erase(tmp.begin());
+        std::vector<Php::Object> phptmp;
+        for(int j=0;j<tmp.size();j++)
+            phptmp.push_back(Php::Object("MicrosunProblem",tmp[j]));
+    return phptmp;
+    }
+    
