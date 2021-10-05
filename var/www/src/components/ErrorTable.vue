82519a3bb3a20fa21cbbb3dd60aaf79b2e338362
Shane <sotiris4321@gmail.com>
2021-10-05 12:59:48 +0300

    fixed new button bug in error table

--- var/www/src/components/ErrorTable.vue
+++ var/www/src/components/ErrorTable.vue
@@ -1,10 +1,10 @@
 <template>
   <h3>{{$store.page_title}}</h3>
   <div class="row">
-      <div class="col-md-1">
+      <div class="col-md-2">
         <input placeholder="filter" type="text" @input="this.onFilterChanged()" v-model="filter">
         </div>
-      <div class="col-md-2">
+      <div class="col-md-1">
           <button type="button" class="btn btn-warning" @click="this.$router.push('newerror');">new</button>
       </div>
   </div>
