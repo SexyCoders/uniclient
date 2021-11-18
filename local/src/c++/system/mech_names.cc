1781f05f97bb6beea56c5c1dca24129bbe145e84
Shane <sotiris4321@gmail.com>
2021-11-18 14:59:52 +0200

    changed getMechNames to getGroupMembers

--- local/src/c++/system/mech_names.cc
+++ local/src/c++/system/mech_names.cc
@@ -1,11 +1,11 @@
 #include <database.h>
 
-Php::Value UniClient::Database::getGroupMembers(std::string group)
+Php::Value UniClient::Database::getGroupMembers(Php::Parameters &group)
     {
         std::vector<std::string> to_return;
         for(unsigned long int j=0;j<this->groups.size();j++)
             {
-                if(this->groups[j].name==group)
+                if(this->groups[j].name==group[0].stringValue())
                     {
                     for(unsigned long int i=0;i<this->groups[j].users.size();i++)
                         {
