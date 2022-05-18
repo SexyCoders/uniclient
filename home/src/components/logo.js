ba8e614c4a41380ba1c73e2f6c2b9a4ca406006f
Shane <sotiris@sexycoders.org>
2022-05-18 14:28:15 +0300

    chaged logo to correct one

--- home/src/components/logo.js
+++ home/src/components/logo.js
@@ -4,18 +4,18 @@ import { Link } from 'components/link';
 
 export default function Logo({ src, ...rest }) {
   return (
-    // <Link
-    //   path="/"
-    //   sx={{
-    //     variant: 'links.logo',
-    //     display: 'flex',
-    //     cursor: 'pointer',
-    //     mr: 15,
-    //   }}
-    //   {...rest}
-    // >
-    //   {/* <Image src={src} alt="startup landing logo" /> */}
-    // </Link>
-    <h1>TheBig4</h1>
+     <Link
+       path="/"
+       sx={{
+         variant: 'links.logo',
+         display: 'flex',
+         cursor: 'pointer',
+         mr: 15,
+       }}
+       {...rest}
+     >
+       <Image src="https://lib.sexycoders.org/logos/uniclient_r_classic_logo_no_bg.png" alt="startup landing logo" style={{width:"60%"}}/> 
+     </Link>
+    //<h1>TheBig4</h1>
   );
 }
